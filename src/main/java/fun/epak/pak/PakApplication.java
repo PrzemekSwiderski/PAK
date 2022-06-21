25682766eb95c9d54bc759ed765aa99fb135eb41
Przemyslaw Swiderski <yt.crysis@gmail.com>
2022-06-21 18:23:10 +0200

    Initial commit

---
+++ src/main/java/fun/epak/pak/PakApplication.java
@@ -0,0 +1,13 @@
+package fun.epak.pak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fun/epak/pak/PakApplicationTests.java
@@ -0,0 +1,13 @@
+package fun.epak.pak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
