6bfc310c38aa6d79b2fef58cc60c272a683c1333
Przemyslaw Swiderski <yt.crysis@gmail.com>
2022-06-27 22:31:58 +0200

    added test data and test jpg

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO `Users` (email, password, username, image_address, register_date, is_active) VALUES('ulfgir@gmail.com', '$2a$10$9YL8li3tMpF0.avaz3lAGegLCrT34xHpLuE47w53ZPPrAHfTcqJ.a', 'Przemek', 'www.google.pl', '2022-06-22', 1);
+INSERT INTO `Users` (email, password, username, image_address, register_date, is_active) VALUES('ulfgir@gmail.com', '$2a$10$9YL8li3tMpF0.avaz3lAGegLCrT34xHpLuE47w53ZPPrAHfTcqJ.a', 'Przemek', 'przemek.jpg', '2022-06-22', 1);
 INSERT INTO User_Role (user_id, role) VALUES(1, 'USER');
 COMMIT;
