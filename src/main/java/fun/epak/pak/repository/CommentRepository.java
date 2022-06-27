f5ff8fc7247714c548353770a4ca018a0f798704
karoljuhnke <kajuhnke@wp.pl>
2022-06-27 10:00:30 +0200

    comment post and service

---
+++ src/main/java/fun/epak/pak/repository/CommentRepository.java
@@ -0,0 +1,9 @@
+package fun.epak.pak.repository;
+
+import fun.epak.pak.model.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
